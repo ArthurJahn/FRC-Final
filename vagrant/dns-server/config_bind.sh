613c6466228d3538656f0e2c0b54d024439848db
Gabriel Araújo <gabriel@startae.com.br>
2015-11-19 14:35:55 -0200

    Setup dnssec vagrant machines.

--- vagrant/dns-server/config_bind.sh
+++ vagrant/dns-server/config_bind.sh
@@ -4,15 +4,29 @@
 apt-get update
 
 # install dns server bind
+echo 'Installing dependencies...'
 apt-get install bind9 dnsutils
+apt-get install haveged
 
 # create zone files
+echo 'Create file zones...'
 mkdir -p /etc/bind/zones/master
-ln -fs /vagrant/db.redesfga.com /etc/bind/zones/master
+cp /vagrant/db.redesfga.com /etc/bind/zones/master
 
-echo "zone \"redesfga.com\" {
-       type master;
-       file \"/etc/bind/zones/master/db.redesfga.com\";
-};" > /etc/bind/named.conf.local
+cd /etc/bind/zones/master
+dnssec-keygen -a NSEC3RSASHA1 -b 2048 -n ZONE resdesfga.com
+dnssec-keygen -f KSK -a NSEC3RSASHA1 -b 4096 -n ZONE resdesfga.com
 
-./etc/init.d/bind9 start
+for key in `ls Kredesfga.com*.key`
+do
+echo "\$INCLUDE $key">> db.redesfga.com
+done
+
+dnssec-signzone -A -3 $(head -c 1000 /dev/random | sha1sum | cut -b 1-16) -N INCREMENT -o redesfga.com -t db.redesfga.com
+
+echo 'Config bind server...'
+cp -f /vagrant/named.conf.local /etc/bind/
+cp -f /vagrant/named.conf.options /etc/bind/
+
+echo 'starting bind server'
+./etc/init.d/bind9 restart
